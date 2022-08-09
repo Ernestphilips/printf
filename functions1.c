fd431d9c37cd011575dbb95c9f4787fa00588adf
Ernestphilips <ernest1philips@gmail.com>
2022-08-09 21:48:07 +0000

    fixed

--- functions1.c
+++ functions1.c
@@ -92,7 +92,7 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789abcdef", buffer,
-				flags, 'x', width, precision, size));
+		flags, 'x', width, precision, size));
 }
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
